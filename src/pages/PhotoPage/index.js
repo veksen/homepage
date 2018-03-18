26fa0afcbef95c1a2abdc12cce0b25149c654b96
Jean-Philippe Sirois <jp@veksen.net>
2018-03-18 14:54:16 -0400

    fix(photography page): bug with current index across categories

--- src/pages/PhotoPage/index.js
+++ src/pages/PhotoPage/index.js
@@ -23,6 +23,15 @@ class PhotoPage extends Component {
     const categories = ["sports", "landscape"];
     const { currentCategory } = this.state;
 
+    // TODO: temp, remove with styled-components
+    const PhotoSliderStyle = {
+      position: "absolute",
+      marginTop: "160px",
+      marginBottom: "60px",
+      left: "75px",
+      right: "75px"
+    };
+
     return (
       <div className="PhotoPage">
         <EmbeddedMenu style={{ position: "absolute", top: "260px", zIndex: "10" }} />
@@ -38,16 +47,13 @@ class PhotoPage extends Component {
             <PhotoMenu categories={categories} currentCategory={currentCategory} />
           </Header>
 
-          <PhotoSlider
-            style={{
-              position: "absolute",
-              marginTop: "160px",
-              marginBottom: "60px",
-              left: "75px",
-              right: "75px"
-            }}
-            photos={currentCategory ? photos.filter(photo => photo.category === this.state.currentCategory) : photos}
-          />
+          {currentCategory === null ? <PhotoSlider style={PhotoSliderStyle} photos={photos} /> : null}
+          {categories.map(category => {
+            const photosByCategory = photos.filter(photo => photo.category === currentCategory);
+            return category === currentCategory ? (
+              <PhotoSlider style={PhotoSliderStyle} photos={photosByCategory} />
+            ) : null;
+          })}
         </div>
       </div>
     );
