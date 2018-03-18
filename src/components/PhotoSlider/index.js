03d829e6f44619cd0911d656a84003d1ce4f8983
Jean-Philippe Sirois <jp@veksen.net>
2018-03-18 01:01:03 -0400

    feat(photography page): make it possible to navigate through photos

--- src/components/PhotoSlider/index.js
+++ src/components/PhotoSlider/index.js
@@ -1,20 +1,58 @@
 import React, { Component } from "react";
 import "./style.css";
+import Arrow from "../../icons/Arrow";
 
 class PhotoSlider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentIndex: 0
+    };
+  }
+
+  previous() {
+    const { currentIndex } = this.state;
+    const { photos } = this.props;
+    const previous = currentIndex === 0 ? photos.length - 1 : currentIndex - 1;
+    this.setState({ currentIndex: previous });
+  }
+
+  next() {
+    const { currentIndex } = this.state;
+    const { photos } = this.props;
+    const nextIndex = photos.length === currentIndex + 1 ? 0 : currentIndex + 1;
+    this.setState({ currentIndex: nextIndex });
+  }
+
   render() {
     const { photos } = this.props;
+    const { currentIndex } = this.state;
 
     return (
       <div {...this.props} className="PhotoSlider">
-        {photos.map(photo => {
-          const image = require(`../../photos/${photo.filename}`);
-          return (
-            <div key={photo.filename} className="PhotoSlider__item">
-              <img className="PhotoSlider__photo" src={image} alt="" />
-            </div>
-          );
-        })}
+        <div className="PhotoSlider__wrapper">
+          <div
+            className="PhotoSlider__previous"
+            onClick={() => this.previous()}
+          >
+            <Arrow direction="left" />
+          </div>
+          <div className="PhotoSlider__next" onClick={() => this.next()}>
+            <Arrow />
+          </div>
+          {photos.map(photo => {
+            const image = require(`../../photos/${photo.filename}`);
+            return (
+              <div
+                key={photo.filename}
+                className="PhotoSlider__item"
+                style={{ left: `-${currentIndex * 100}%` }}
+              >
+                <img className="PhotoSlider__photo" src={image} alt="" />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
