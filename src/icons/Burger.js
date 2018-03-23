92fb73682b92988fb82cd71f04560321ffdc4150
Jean-Philippe Sirois <jp@veksen.net>
2018-03-23 01:59:14 -0400

    fix: typo in Burger icon class name

--- src/icons/Burger.js
+++ src/icons/Burger.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Arrow = () => {
+const Burger = () => {
   return (
     <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
       <path d="M0 0h16v2H0zM0 7h16v2H0zM0 14h16v2H0z" />
@@ -8,4 +8,4 @@ const Arrow = () => {
   );
 };
 
-export default Arrow;
+export default Burger;
