df447fab0592436d13f445f9603ff26140dbe4f1
Jean-Philippe Sirois <jp@veksen.net>
2018-03-28 17:48:24 -0400

    feat: add GA tracking, closes #1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+ReactGA.initialize("UA-47549558-1");
 
 ReactDOM.render(
   <BrowserRouter>
