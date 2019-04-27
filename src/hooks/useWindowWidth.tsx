8308ad7241e45b0679bf8df7cfc615bcf47e51df
Jean-Philippe Sirois <jp@veksen.net>
2019-04-26 22:22:16 -0400

    build: fix broken build because window is not defined

--- src/hooks/useWindowWidth.tsx
+++ src/hooks/useWindowWidth.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 
 export function useWindowWidth(): number {
+  // for an env without window (on build), prevent failure
+  if (typeof window === "undefined") {
+    return 0
+  }
+
   const [width, setWidth] = useState(window.innerWidth)
 
   useEffect(() => {
