36aeb2ef2418ab68a7ffb211c5e06284fea7abc6
Jean-Philippe Sirois <jp@veksen.net>
2019-05-09 23:22:38 -0400

    content: update manifest metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jean-Philippe Sirois - Web developer`,
+        short_name: `veksen.com`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
